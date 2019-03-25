5c9a44506302e8e658efec6cb0c3240101ded8c6
Nam Koong Kwon <43670900+kkoon9@users.noreply.github.com>
2019-03-25 14:36:52 +0900

    Update HX711.c
    로드셀마다 offset과 correct 값이 달라서 코드를 수정하였다.

--- HX711/HX711.c
+++ HX711/HX711.c
@@ -1,64 +1,81 @@
 #include <HX711.h>
 
-double a_Offset = -8062.83;
-double a_correct = 0.00303;
-double b_Offset = -131681.18;
-double b_correct = 0.00306;
-double c_Offset = -94007.92;
-double c_correct = 0.00306;
-double d_Offset = -126868.67;
-double d_correct = 0.00223;
-double e_Offset = -35743.56;
-double e_correct = 0.00315;
-HX711 hx_a(A3, A2, 128, a_correct);
-HX711 hx_b(9, 6, 128, b_correct);
-HX711 hx_c(5, 3, 128, c_correct);
-HX711 hx_d(A1, A0, 128, d_correct);
-HX711 hx_e(A5, A4, 128, e_correct);
+double a_Offset = 274656.84;
+double a_correct = 0.0023;
+double b_Offset = -236480.81;
+double b_correct = 0.00235;
+double c_Offset = -75999.82;
+double c_correct = 0.00238;
+double d_Offset = -136521.81;
+double d_correct = 0.0025;
+double e_Offset = 69528.19;
+double e_correct = 0.002355;
+HX711 hx_a(A0, A1, 128, a_correct);
+HX711 hx_b(6, 9, 128, b_correct);
+HX711 hx_c(A2, A3, 128, c_correct);
+HX711 hx_d(A4, A5, 128, d_correct);
+HX711 hx_e(3, 5, 128, e_correct);
 
 int a_sum1 = 0, b_sum1 = 0, c_sum1 = 0, d_sum1 = 0, e_sum1 = 0;
 int flag = 0;
+
 void setup() {
   Serial.begin(9600);
+  weightCorrect();
 }
-void loop()
-{
-  if (flag == 2)
-    weightPrint();
 
-  if (Serial.available() && flag == 0) {
-    flag = 1;
-  }
-  else if (flag == 1) {
-    weightCorrect();
-    flag = 2;
-    Serial.write('Z');
-  }
+void loop() {
+
+  weightPrint();
+
+  //if (Serial.available() && flag == 0) {
+  //  flag = 1;
+  //}
+  //else if (flag == 1) {
+  //  weightCorrect();
+  //  flag = 2;
+  //  Serial.write('Z');
+  //}
 
 }
 void weightPrint() {
   a_sum1 = 0, b_sum1 = 0, c_sum1 = 0, d_sum1 = 0, e_sum1 = 0;
   a_sum1 = hx_a.bias_read();
   Serial.print("A");
+  if (a_sum1 < 0)
+    a_sum1 -= a_sum1;
   Serial.print(a_sum1);
 
   b_sum1 = hx_b.bias_read();
   Serial.print("B");
+  if (b_sum1 < 0)
+    b_sum1 -= b_sum1;
+
   Serial.print(b_sum1);
 
   c_sum1 = hx_c.bias_read();
   Serial.print("C");
+  if (c_sum1 < 0)
+    c_sum1 -= c_sum1;
+
   Serial.print(c_sum1);
 
   d_sum1 = hx_d.bias_read();
   Serial.print("D");
+  if (d_sum1 < 0)
+    d_sum1 -= d_sum1;
+
   Serial.print(d_sum1);
 
   e_sum1 = hx_e.bias_read();
   Serial.print("E");
-  Serial.print(e_sum1);
-  delay(3000);
+  if (e_sum1 < 0)
+    e_sum1 -= e_sum1;
+
+  Serial.println(e_sum1);
+  delay(5000);
 }
+
 void weightCorrect() { // 무게 보정
 
   hx_a.set_offset(a_Offset);
@@ -118,6 +135,6 @@ void weightCorrect() { // 무게 보정
     hx_c.set_offset(c_Offset);
     hx_d.set_offset(d_Offset);
     hx_e.set_offset(e_Offset);
-    delay(1000);
+
   } while (a_sum1 != 0 || b_sum1 != 0 || c_sum1 != 0 || d_sum1 != 0 || e_sum1 != 0);
 }
